26a12bfbb013b9046cbb8d6e02d48ce491df2235
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2022-08-24 15:01:56 +0200

    A bunch of additions.

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,112 @@
 <script>
-  import Counter from './lib/Counter.svelte';
 </script>
 
-<main>
-  <h1>Sammanslaget grupp 4</h1>
+<nav>
+  <ul>
+    <li><a href="#hem">Namn</a></li>
+    <li><a href="#game">Spela</a></li>
+    <li><a href="#store">Gör skillnad</a></li>
+    <li><a href="#about">Om oss</a></li>
+  </ul>
+</nav>
 
-  <div class="card">
-    <Counter />
-  </div>
+<main class="scroller">
+  <section class="header">
+    <div class="header-cloud-div-orange">
+      <img
+        class="header-cloud header-cloud-orange"
+        src="/public/cloud_orange_new.png"
+        alt="Decorative orange cloud"
+      />
+    </div>
+    <div class="header-text">
+      <h1>Hinner du med i trendcykeln?</h1>
+      <h2>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris.
+      </h2>
+      <img class="arrow" src="/public/arrow.png" alt="Arrow down symbol" />
+    </div>
+    <div>
+      <div class="header-cloud-div-green">
+        <img
+          class="header-cloud header-cloud-green"
+          src="/public/cloud_green_new.png"
+          alt="Decorative green cloud"
+        />
+      </div>
+    </div>
+  </section>
+
+  <section id="game" class="game">
+    <div>
+      <h2>Spelet</h2>
+    </div>
+  </section>
+
+  <section class="game-info">
+    <div>
+      <div>
+        <img src="#" alt="" />
+        <h3>Lorem ipsum</h3>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <img src="#" alt="" />
+      </div>
+      <div>
+        <img src="#" alt="" />
+        <h3>Lorem ipsum</h3>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <img src="#" alt="" />
+      </div>
+      <div>
+        <img src="#" alt="" />
+        <h3>Lorem ipsum</h3>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <img src="#" alt="" />
+      </div>
+      <div />
+    </div>
+  </section>
+
+  <section id="store" class="stores">
+    <div>
+      <h2>Gör skillnad</h2>
+      <img src="#" alt="" />
+      <h4>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </h4>
+      <button>Hitta butik</button>
+      <img src="#" alt="" />
+      <img src="#" alt="" />
+      <img src="#" alt="" />
+    </div>
+  </section>
+
+  <section id="about" class="about">
+    <h2>Om oss</h2>
+    <h4>
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris.
+    </h4>
+    <img src="#" alt="" />
+    <img src="#" alt="" />
+    <img src="#" alt="" />
+  </section>
 </main>
+
+<footer />
 
 <style>
 </style>
